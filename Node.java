f60d1dfed0949640fe7b53525915334099dae446
jzhong10 <junzhong2003@gmail.com>
2021-01-06 13:07:20 -0500

    initial code for a Node

---
+++ Node.java
@@ -0,0 +1,15 @@
+public class Node {
+  
+ private String data;
+ private Node next,prev;
+ //write get/set methods for all three instance variables.
+ 
+ public Node (String value) {
+   data = value;
+ }
+ 
+ public String get() {
+   return value;
+ }
+ 
+}
