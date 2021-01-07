d2ee08ef7399b27b4d27c8a8405867f617a9fcbf
jzhong10 <junzhong2003@gmail.com>
2021-01-06 20:36:23 -0500

    Fixed method add() to link end chains, implemented helper function to return the node at index n and consequently method get(index n)

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -6,6 +6,7 @@ public class MyLinkedList {
     size = 0;
     start = new Node();
     end = new Node();
+    start.setNext(end);
   }
 
   public int size() {
@@ -18,21 +19,36 @@ public class MyLinkedList {
       start = n;
       end = n;
     } else if (size==1) {
-        end = n;
+        start.setNext(n);
+        end = start.getNext();
     } else {
         end.setNext(n);
+        end = end.getNext();
     }
     size++;
     return true;
   }
   
+  public String get(int index) {
+    Node n = getNode(index);
+    return n.get();
+  }
+  
   /*
   public boolean add(int index, String value);
-  public String get(int index);
   public String set(int index, String value);
   public String toString() {
-    
   }
   */
+
   //Any helper method that returns a Node object MUST BE PRIVATE!
+  private Node getNode(int index) {
+    int count = 0;
+    Node n = start;
+    while (count<index) {
+      n = n.getNext();
+      count++;
+    }
+    return n;
+  }
 }
